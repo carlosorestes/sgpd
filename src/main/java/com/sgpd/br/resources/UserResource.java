c7b9947f54515c7502c21c6abe3a7a3003cb7342
carlosmalaquias <carlosomalaquias@gmail.com>
2019-08-17 19:35:24 -0300

    Adicionando Camada Entity User e Resource

---
+++ src/main/java/com/sgpd/br/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.sgpd.br.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sgpd.br.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User user = new User(1L, "carlos orestes", "999999999", "123456");
+		return ResponseEntity.ok().body(user);
+	}
+
+}
