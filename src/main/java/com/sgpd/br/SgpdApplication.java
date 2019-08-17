4293b6fb442962d5e4924e1c187e334d78e6e5c4
carlosmalaquias <carlosomalaquias@gmail.com>
2019-08-17 19:05:05 -0300

    Inicio Projeto

---
+++ src/main/java/com/sgpd/br/SgpdApplication.java
@@ -0,0 +1,13 @@
+package com.sgpd.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpdApplication.class, args);
+	}
+
+}
